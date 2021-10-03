53123df967c89a651390a6f20c4ef503ae124447
alextech <alex@alex-tech-adventures.com>
2021-10-04 00:44:11 +0300

    #chapter_1 coordinates addition and subtraction

--- Catch_MathTest/Catch_MathTest.cpp
+++ Catch_MathTest/Catch_MathTest.cpp
@@ -5,7 +5,7 @@
 
 using namespace rt_math;
 
-SCENARIO("A tuple with w=1.0 is a point", "[geometry]") {
+SCENARIO("A tuple with w=1.0 is a point", "[coordinates]") {
 
     GIVEN("a tuple") {
 
@@ -33,7 +33,7 @@ SCENARIO("A tuple with w=1.0 is a point", "[geometry]") {
     }
 }
 
-SCENARIO("point() creates tuples with w = 1", "[geometry]")
+SCENARIO("point() creates tuples with w = 1", "[coordinates]")
 {
 	GIVEN("p <- point(4, -4, 3")
 	{
@@ -46,15 +46,122 @@ SCENARIO("point() creates tuples with w = 1", "[geometry]")
 	}
 }
 
-SCENARIO("vector() creates tuples with w = 0", "[geometry]")
+SCENARIO("vector() creates tuples with w = 0", "[coordinates]")
 {
     GIVEN("p <- vector(4, -4, 3")
     {
 	    const tuple p = vector(4, -4, 3);
 
-        THEN("p = tuple(4, -4, 3, 0")
+        THEN("p = tuple(4, -4, 3, 0)")
         {
             REQUIRE(p == tuple(4, -4, 3, 0));
         }
     }
+}
+
+/*
+ * In geometry, vector + point = vector; vector + vector = vector; point + point = doesnt make sense
+ */
+SCENARIO("Adding two tuples", "[coordinates]")
+{
+	GIVEN("a1 <- tuple(3, -2, 5, 1)")
+	{
+		const tuple a1 = tuple(3, -2, 5, 1);
+
+		AND_GIVEN("a2 <- tuple(-2, 3, 1, 0)")
+		{
+			const tuple a2 = tuple(-2, 3, 1, 0);
+
+			THEN("a1 + a2 = tuple(1, 1, 6, 1)")
+			{
+				const tuple sum = a1 + a2;
+
+				REQUIRE(sum == tuple(1, 1, 6, 1));
+			}
+		}
+	}
+}
+
+/*
+ * In geometry, point - point = vector. Direction vector how to get from first point to second.
+ */
+SCENARIO("Subtracting two points", "[coordinates]")
+{
+	GIVEN("p1 <- point(3, 2, 1)")
+	{
+		const tuple p1 = point(3, 2, 1);
+
+		AND_GIVEN("p2 <- point(5, 6, 7)")
+		{
+			const tuple p2 = point(5, 6, 7);
+
+			THEN("p1 - p2 = vector(-2, -4, -6)")
+			{
+				const tuple v = p1 - p2;
+
+				REQUIRE(v == vector(-2, -4, -6));
+			}
+		}
+	}
+}
+
+/*
+ * In geometry, point - vector = point. New point using direction vector from original point.
+ */
+SCENARIO("Subtracting a vector from a point", "[coordinates]")
+{
+	GIVEN("p <- point(3, 2, 1)")
+	{
+		const tuple p = point(3, 2, 1);
+
+		AND_GIVEN("v <- vector(5, 6, 7)")
+		{
+			const tuple v = vector(5, 6, 7);
+
+			REQUIRE(p - v == point(-2, -4, -6));
+		}
+	}
+}
+
+/*
+ * In geometry, vector - vector = vector. New vector as change of direction between two.
+ */
+SCENARIO("Subtracting two vectors", "[coordinates]")
+{
+	GIVEN("v1 <- vector(3, 2, 1)")
+	{
+		const tuple v1 = vector(3, 2, 1);
+
+		AND_GIVEN("v2 <- vector(5, 6, 7)")
+		{
+			const tuple v2 = vector(5, 6, 7);
+
+			REQUIRE(v1 - v2 == vector(-2, -4, -6));
+		}
+	}
+}
+
+SCENARIO("Subtracting a vector from a zero vector", "[coordinates]")
+{
+	GIVEN("zero <- vector(0, 0, 0)")
+	{
+		const tuple zero = vector(0, 0, 0);
+
+		AND_GIVEN("v <- vector(1, -2, 3")
+		{
+			const tuple v = vector(1, -2, 3);
+
+			REQUIRE(zero - v == vector(-1, 2, -3));
+		}
+	}
+}
+
+SCENARIO("Negating a tuple", "[coordinates]")
+{
+	GIVEN("a <- tuple(1, -2, 3, -4")
+	{
+		const tuple a = tuple(1, -2, 3, -4);
+
+		REQUIRE(-a == tuple(-1, 2, -3, 4));
+	}
 }
--- Math/Math.h
+++ Math/Math.h
@@ -26,7 +26,7 @@ namespace rt_math
 			return w == 0.0f;
 		}
 
-		friend bool operator==(const tuple& lhs, const tuple& rhs)
+		friend bool operator==(const tuple &lhs, const tuple &rhs)
 		{
 			return eq_f(lhs.x, rhs.x)
 				&& eq_f(lhs.y, rhs.y)
@@ -34,9 +34,41 @@ namespace rt_math
 				&& eq_f(lhs.w, rhs.w);
 		}
 
-		friend bool operator!=(const tuple& lhs, const tuple& rhs)
+		friend bool operator!=(const tuple &lhs, const tuple &rhs)
 		{
 			return !(lhs == rhs);
+		}
+
+		/*
+		 * This is a good example of why class inheritance for Point and Vector types do not make sense.
+		 *
+		 * Adding w=1 (point) to a w=0 (vector) produces a point.
+		 * Adding w=0 (vector) to a w=0 (vector) produces another vector.
+		 * Adding w=1 (point) to a w=1 (point) produces w=2, undefined here,
+		 *   which matches the fact that mathematically cannot add point and a point.
+		 */
+		friend tuple operator+(const tuple &lhs, const tuple &rhs)
+		{
+			return tuple(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w);
+		}
+
+		/*
+		 * Subtracting w=1 (point) - w=1 (point) gives w=0 (vector), as expected mathematically,
+		 *   giving __direction__ between two points.
+		 *
+		 * Useful for chapter 6 - finding vector pointing to light source.
+		 */
+		friend tuple operator-(const tuple &lhs, const tuple &rhs)
+		{
+			return tuple(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w);
+		}
+
+		/*
+		 * Negating vector. Useful for chapter 6 - shading.
+		 */
+		friend tuple operator-(const tuple &rhs)
+		{
+			return tuple(0 - rhs.x, 0 - rhs.y, 0 - rhs.z, 0 - rhs.w);
 		}
 	};
 
